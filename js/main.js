2f84c8acd2b8e86963f2ee3fb1fad89d22d808f2
Ivan Tihonov <ivan.tixonov000@gmail.com>
2024-09-06 22:48:19 +0700

    Глаз для пароля сделан

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
+const passwordWrapper = document.querySelector('.password_wrapper');
+const passwordInput = document.getElementById('password');
+
+function changePasswordWidth() {
+    const passwordInputWidth = passwordInput.clientWidth;
+    passwordWrapper.style.width = passwordInputWidth + "px";
+}
+
+window.addEventListener('DOMContentLoaded', changePasswordWidth);
+
+window.addEventListener('resize', () => {
+    passwordWrapper.style.width = "100%";
+    changePasswordWidth();
+});
+
+const passwordEye = document.querySelector('.password_eye');
+passwordEye.addEventListener('click', () => {
+    if (passwordEye.classList.contains('password_hide')) {
+        passwordInput.type = 'text';
+        passwordEye.classList.remove('password_hide');
+        passwordEye.classList.add('password_show');
+    } else {
+        passwordInput.type = 'password';
+        passwordEye.classList.remove('password_show');
+        passwordEye.classList.add('password_hide');
+    }
+});
+
 const tel = document.getElementById("tel");
 const error = document.querySelector(".error");
-
 forError = tel.clientWidth / 2 + 20 + "px";
-
 error.style.left = `calc(50% + ${forError})`;
-
-const passwordInput = document.getElementById('password');
