aa71927cd677e47f11457a49c6be56685f418f86
Ivan Tihonov <ivan.tixonov000@gmail.com>
2024-09-08 22:41:57 +0700

    Добавлена Google Аналитика

--- pages/register.php
+++ pages/register.php
@@ -13,6 +13,19 @@ if (isset($_SESSION['user'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-B8VDZ6MFS5"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'G-B8VDZ6MFS5');
+    </script>
     <link rel="stylesheet" href="../css/register.css">
     <title>Регистрация</title>
 </head>
